db2b0f2ee13954e4d810ecdd7bcdd113e11fb54a
HyeongJu Yoo <zerofunc@naver.com>
2018-08-01 00:49:52 +0900

    다른 웹서버 사용 및 포트변경

--- springbootgettingstarted/src/main/java/me/hj/Application.java
+++ springbootgettingstarted/src/main/java/me/hj/Application.java
@@ -1,17 +1,11 @@
 package me.hj;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(Application.class);
-		springApplication.setWebApplicationType(WebApplicationType.NONE);
-		springApplication.run(args);
+		public static void main(String[] args) {
+			SpringApplication.run(Application.class, args);
 	}
 }
