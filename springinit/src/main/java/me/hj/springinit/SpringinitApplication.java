8802bbba18f190cc42304f3c1cf1cd7b3959deea
HyeongJu Yoo <zerofunc@naver.com>
2018-08-04 23:49:28 +0900

    Set WebApplicationtType 'SERVLET'
    웹어플리케이션 타입을 SERVLET으로 설정

--- springinit/src/main/java/me/hj/springinit/SpringinitApplication.java
+++ springinit/src/main/java/me/hj/springinit/SpringinitApplication.java
@@ -1,6 +1,7 @@
 package me.hj.springinit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -9,6 +10,7 @@ public class SpringinitApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringinitApplication.class);
+        app.setWebApplicationType(WebApplicationType.SERVLET);
         app.run(args);
     }
 }
