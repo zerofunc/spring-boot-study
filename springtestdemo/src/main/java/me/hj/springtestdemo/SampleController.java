83ea790ae208045e08b4b5e63ee55001e849564c
HyeongJu Yoo <zerofunc@naver.com>
2018-08-24 00:45:15 +0900

    Add OutputCapture
    로그를 비롯해 콘솔에 찍히는 모든걸 캡쳐함.

--- springtestdemo/src/main/java/me/hj/springtestdemo/SampleController.java
+++ springtestdemo/src/main/java/me/hj/springtestdemo/SampleController.java
@@ -1,5 +1,7 @@
 package me.hj.springtestdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    Logger logger = LoggerFactory.getLogger(SampleController.class);
+
     @Autowired
     private SampleService sampleService;
 
     @GetMapping("/hello")
     public String hello() {
+        logger.info("hoho");
+        System.out.println("skip");
         return "hello" + sampleService.getName();
     }
 
--- springtestdemo/src/test/java/me/hj/springtestdemo/SampleControllerTest.java
+++ springtestdemo/src/test/java/me/hj/springtestdemo/SampleControllerTest.java
@@ -1,5 +1,6 @@
 package me.hj.springtestdemo;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -24,6 +26,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
+    //junit 확장
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
     @MockBean
     SampleService mockSampleService;
 
@@ -36,5 +42,9 @@ public class SampleControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("helloholly"));
+
+        assertThat(outputCapture.toString())
+                .contains("hoho")
+                .contains("skip");
     }
 }
