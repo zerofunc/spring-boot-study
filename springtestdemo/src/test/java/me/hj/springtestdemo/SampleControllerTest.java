8a90d480a23c07f83845d4d9e7bc32a6394a0e8c
HyeongJu Yoo <zerofunc@naver.com>
2018-08-24 00:34:09 +0900

    Add WebTestClient
    webflux 추가해서 WebTestClient로 테스트하기

--- springtestdemo/src/test/java/me/hj/springtestdemo/SampleControllerTest.java
+++ springtestdemo/src/test/java/me/hj/springtestdemo/SampleControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SampleControllerTest {
 
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
@@ -31,7 +32,8 @@ public class SampleControllerTest {
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("holly");
 
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("helloholly");
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("helloholly");
     }
 }
