f768a2f49e7e86ca0030ce88ade403ee87f3d4f3
HyeongJu Yoo <zerofunc@naver.com>
2018-08-24 00:25:07 +0900

    Add SampleControllerTest
    TestRestTemplate 사용해보기

---
+++ springtestdemo/src/main/java/me/hj/springtestdemo/SampleController.java
@@ -0,0 +1,19 @@
+package me.hj.springtestdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @Autowired
+    private SampleService sampleService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello" + sampleService.getName();
+    }
+
+
+}
---
+++ springtestdemo/src/main/java/me/hj/springtestdemo/SampleService.java
@@ -0,0 +1,11 @@
+package me.hj.springtestdemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "hyeongJu";
+    }
+}
