aae33e4d70707f522d0862cfc701fc39dab9e228
HyeongJu Yoo <zerofunc@naver.com>
2018-08-26 23:03:58 +0900

    Init SpringMvcDemo Project

---
+++ springmvcdemo/src/test/java/my/hj/springmvcdemo/SpringmvcdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package my.hj.springmvcdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringmvcdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
