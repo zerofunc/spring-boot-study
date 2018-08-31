f6799ae5c861c23f3f9ccdac4f15b7d51d0b15ec
HyeongJu Yoo <zerofunc@naver.com>
2018-08-31 23:14:58 +0900

    Init Spring boot thymeleaf demo project

---
+++ springbootthymeleaf/src/main/java/me/hj/springbootthymeleaf/SpringbootthymeleafApplication.java
@@ -0,0 +1,12 @@
+package me.hj.springbootthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootthymeleafApplication.class, args);
+    }
+}
