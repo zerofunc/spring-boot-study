690d86d6c7e58121ca2b525b7cb82fd5b3806c6c
HyeongJu Yoo <zerofunc@naver.com>
2018-09-04 23:42:41 +0900

    Init Spirngboothateoas project

---
+++ springboothateoas/src/main/java/my/hj/springboothateoas/SpringboothateoasApplication.java
@@ -0,0 +1,12 @@
+package my.hj.springboothateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothateoasApplication.class, args);
+    }
+}
