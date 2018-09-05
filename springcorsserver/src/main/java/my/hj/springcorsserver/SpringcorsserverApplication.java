0137de34672e869aaabb342ce2e7e87fe56b3717
HyeongJu Yoo <zerofunc@naver.com>
2018-09-05 23:54:52 +0900

    Add hello api

--- springcorsserver/src/main/java/my/hj/springcorsserver/SpringcorsserverApplication.java
+++ springcorsserver/src/main/java/my/hj/springcorsserver/SpringcorsserverApplication.java
@@ -1,10 +1,19 @@
 package my.hj.springcorsserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringcorsserverApplication {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcorsserverApplication.class, args);
