5e5487a022ff8e5bedd0d2271fa566b997b62ce9
HyeongJu Yoo <zerofunc@naver.com>
2018-09-05 23:29:57 +0900

    Init springcorsclient project

---
+++ springcorsclient/src/main/java/my/hj/springcorsclient/SpringcorsclientApplication.java
@@ -0,0 +1,12 @@
+package my.hj.springcorsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcorsclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcorsclientApplication.class, args);
+    }
+}
