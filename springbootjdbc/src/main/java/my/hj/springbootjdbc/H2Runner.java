8649c58f304acd4b54eb7cdcef2eaee0ce3a79f2
HyeongJu Yoo <zerofunc@naver.com>
2018-09-06 23:54:48 +0900

    Use Spring h2 console

--- springbootjdbc/src/main/java/my/hj/springbootjdbc/H2Runner.java
+++ springbootjdbc/src/main/java/my/hj/springbootjdbc/H2Runner.java
@@ -3,6 +3,7 @@ package my.hj.springbootjdbc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -15,15 +16,19 @@ public class H2Runner implements ApplicationRunner {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public void run(ApplicationArguments args) throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-            connection.getMetaData().getURL();
-            connection.getMetaData().getUserName();
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
 
             Statement statement = connection.createStatement();
             String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
             statement.executeUpdate(sql);
         }
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'hyeongju')");
     }
 }
