426e7834b967d6f4e3fe7d25525755fbaf136539
HyeongJu Yoo <zerofunc@naver.com>
2018-09-06 23:40:47 +0900

    Init Springbootjdbc project

---
+++ springbootjdbc/src/main/java/my/hj/springbootjdbc/SpringbootjdbcApplication.java
@@ -0,0 +1,12 @@
+package my.hj.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjdbcApplication.class, args);
+    }
+}
