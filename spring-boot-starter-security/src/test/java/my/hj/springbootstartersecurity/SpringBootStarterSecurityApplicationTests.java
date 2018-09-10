711eca911826c3a2d1bb916a1392f57402306426
HyeongJu Yoo <zerofunc@naver.com>
2018-09-10 23:37:26 +0900

    Init Spring-Boot-Starter-Security Project

---
+++ spring-boot-starter-security/src/test/java/my/hj/springbootstartersecurity/SpringBootStarterSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package my.hj.springbootstartersecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootStarterSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
