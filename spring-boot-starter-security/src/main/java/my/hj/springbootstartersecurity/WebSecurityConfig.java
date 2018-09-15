f399a1f983ce2a6343e20efa255477b98b62c1dd
HyeongJu Yoo <zerofunc@naver.com>
2018-09-15 23:04:34 +0900

    Add WebScurityConfig

---
+++ spring-boot-starter-security/src/main/java/my/hj/springbootstartersecurity/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package my.hj.springbootstartersecurity;
+
+import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig extends SpringBootWebSecurityConfiguration {
+
+}
